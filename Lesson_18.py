5624e2d27f2f4da3e01d2d3942b08894a86e91f9
Pasha Kozachenko <pasha22151521@gmail.com>
2023-02-03 16:46:09 +0200

    Lesson 18
    * __add__, __sub__, __eq__, __hash__

---
+++ Lesson_18.py
@@ -0,0 +1,37 @@
+class Test:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return self.value + other
+
+    def __sub__(self, other):
+        return self.value - other
+
+    def __eq__(self, other):
+        if other == self.value:
+            return True
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+class P:
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        return 100
+
+    def __sub__(self, other):
+        return 100
+
+    def __eq__(self, other):
+        return self.value == other
+
+    def __hash__(self):
+        sh = hash(self)
+        print('self.__hash__:', sh)
+        return sh
