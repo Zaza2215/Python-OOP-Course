729b280579491ad422ff21424adde040c1da9933
Pasha Kozachenko <pasha22151521@gmail.com>
2023-02-03 15:21:52 +0200

    Lesson 16
    * __next__, __iter__, __call__

---
+++ Lesson_16.py
@@ -0,0 +1,27 @@
+class User:
+    def __init__(self, value):
+        self.value = value
+
+    def __call__(self, comment):
+        print('__call__:', self.value)
+        print('comment:', comment)
+
+    def __iter__(self, other=0):
+        print('__iter__')
+        if not ('array' in self.__dict__.keys()) or not self.array:
+            self.array = [i for i in range(other, 0, -1)]
+        return self
+
+    def __next__(self):
+        if self.array:
+            return f'__next__: {self.array.pop()}'
+        else:
+            raise StopIteration('self.array is empty')
+
+
+if __name__ == '__main__':
+    a = User(12)
+    a('Hello')
+    b = a.__iter__(5)
+    for i in b:
+        print(i)
