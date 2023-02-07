fe29113eeb5317289af36c44f637e31d3829d3bd
Pasha Kozachenko <pasha22151521@gmail.com>
2023-02-07 14:25:35 +0200

    Lesson 32
    * property
    * psutil

---
+++ Lesson_32.py
@@ -0,0 +1,27 @@
+import psutil
+
+
+class Test:
+    def __init__(self):
+        self.__process = None
+        self.__cpu_count = None
+
+    @property
+    def process(self):
+        if self.__process:
+            return self.__process
+        else:
+            return 'There is no data'
+
+    @property
+    def cpu_count(self):
+        if self.__cpu_count:
+            return self.__cpu_count
+        else:
+            return 'There is no data'
+
+    def get_process(self):
+        self.__process = psutil.process_iter()
+
+    def get_cpu(self):
+        self.__cpu_count = psutil.cpu_count()
